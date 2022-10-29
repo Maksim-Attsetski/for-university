0f47c03a9618e0ded9b0409c6d791e5a0501fcaa
Maksim-Attsetski <maks2001maks2001@gmail.com>
2022-10-30 00:01:04 +0300

    update build

---
+++ src/secret.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCI9g6baPTh3-WbSLhg3zPvkEC1s7F5Uno',
+  authDomain: 'admin-dashboard-74346.firebaseapp.com',
+  databaseURL: 'https://admin-dashboard-74346-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'admin-dashboard-74346',
+  storageBucket: 'admin-dashboard-74346.appspot.com',
+  messagingSenderId: '140561293917',
+  appId: '1:140561293917:web:6e19198ad14271f656a3b4',
+  measurementId: 'G-RR2K2QLJWZ',
+};
+
+export { firebaseConfig };
