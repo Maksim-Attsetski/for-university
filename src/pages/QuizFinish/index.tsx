4fce9b92877f80eb32c8358c43b9e29312516ed0
Maksim Attsetski <maks2001maks2001@gmail.com>
2022-12-03 00:45:49 +0300

    feature: calc total price by quiz results

--- src/pages/QuizFinish/index.tsx
+++ src/pages/QuizFinish/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
@@ -8,22 +8,56 @@ import { useActions } from '../../hooks/useActions';
 
 // import s from './QuizFinish.module.scss';
 
+interface ITotal {
+  price: number;
+}
+
 const QuizFinish: FC = () => {
   const { answers } = useTypedSelector(state => state.quiz);
+  const { systems } = useTypedSelector(state => state.systems);
+  const [total, setTotal] = useState<ITotal>({ price: 0 });
   const { action } = useActions();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (answers.length === 0) {
-      navigate(routes.quizBig);
-      action.startQuiz();
+  const setTotalPrice = () => {
+    let systemsInAnswers = [];
+
+    for (let i = 0; i < answers.length; i++) {
+      const answer = answers[i];
+
+      for (let j = 0; j < systems.length; j++) {
+        const system = systems[j];
+        if (system.id === answer.answer?.systemId) {
+          systemsInAnswers.push(system);
+        }
+      }
     }
+
+    const price = systemsInAnswers.reduce((prev, cur) => (prev += cur.price), 0);
+
+    setTotal(prev => ({ ...prev, price }));
+  };
+
+  const resetQuiz = () => {
+    navigate(routes.quizBig);
+    action.startQuiz();
+  };
+
+  useEffect(() => {
+    answers.length === 0 ? resetQuiz() : setTotalPrice();
   }, []);
 
   return (
     <div>
       <br />
       <div className="container">
+        <div className="flex gap-4 flex-wrap items-center">
+          <div>
+            <strong>Full price for all custom systems</strong>
+          </div>
+          <div className="px-2 py-1 bg-title-color text-white w-max rounded-md">{total.price}</div>
+        </div>
+        <br />
         {answers.map(
           answer =>
             answer && (
