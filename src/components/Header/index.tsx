68df1b3573bbec310e0442a747743852b55c5edb
Maksim Attsetski <maks2001maks2001@gmail.com>
2022-12-15 19:53:19 +0300

    refactor: rewrite navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,105 +1,18 @@
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-
-import { useTypedSelector } from '../../hooks/redux';
-import { useAuth } from '../../hooks/useAuth';
-import { routes } from '../../data';
-
-import Button from '../Button';
 import NavBar from '../NavBar';
 import Logo from '../Logo';
-import Select, { IOption } from '../Select';
 
 import s from './Header.module.scss';
-import { images } from '../../assets';
-
-interface ILink {
-  to: routes;
-  name: string;
-  isActive: boolean;
-}
 
 const Header: FC = () => {
-  const { pathname } = useLocation();
-  const { isAuth, currentUser } = useTypedSelector(state => state.auth);
-  const { onLogOutBtnClick } = useAuth();
-  const navigate = useNavigate();
-  const checkIsActive = (link: routes) => pathname === link;
-
-  const avatar = useMemo(
-    () => (!!currentUser?.photoURL ? currentUser.photoURL : images.profile),
-    [currentUser?.photoURL],
-  );
-
-  const links: ILink[] = useMemo(
-    () => {
-      const allRoutes: ILink[] = [
-        { to: routes.about, name: 'О NumBer', isActive: checkIsActive(routes.about) },
-        { to: routes.catalog, name: 'Каталог', isActive: checkIsActive(routes.catalog) },
-        { to: routes.contacts, name: 'Наши контакты', isActive: checkIsActive(routes.contacts) },
-      ];
-
-      const privateRoutes: ILink[] = [
-        // { to: routes.projects, name: 'Мои проекты', isActive: checkIsActive(routes.projects) },
-      ];
-
-      return isAuth ? [...allRoutes, ...privateRoutes] : [...allRoutes];
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [pathname],
-  );
-
-  const otherButtons: ILink[] = useMemo(
-    () => [
-      { to: routes.profile, name: 'Аккаунт', isActive: checkIsActive(routes.profile) },
-      { to: routes.projects, name: 'Мои проекты', isActive: checkIsActive(routes.projects) },
-      { to: routes.exchangeRate, name: 'Курсы валют', isActive: checkIsActive(routes.exchangeRate) },
-    ],
-    [],
-  );
-
-  const options: IOption[] = useMemo(
-    () => [
-      { title: 'Аккаунт', onClick: () => navigate(routes.profile), icon: null },
-      { title: 'Мои проекты', onClick: () => navigate(routes.projects), icon: null },
-      { title: 'Курсы валют', onClick: () => navigate(routes.exchangeRate), icon: null },
-      { title: 'Выйти', onClick: onLogOutBtnClick, isButton: true, icon: null },
-    ],
-    [],
-  );
-
   return (
     <header className={s.header}>
       <div className={s.blur} />
       <div className={'container ' + s.headerBody}>
         <Logo />
         <div className={s.headerContent}>
-          <div className="links">
-            {links.map(({ to, name, isActive }) => (
-              <Link key={to} to={to} className={`${isActive ? s.active : ''} ${s.link}`}>
-                {name}
-                <span className={s.linkLine} />
-              </Link>
-            ))}
-          </div>
-          <div className="burger">
-            <NavBar links={links.concat(otherButtons)} />
-          </div>
-          {isAuth ? (
-            <div className="links">
-              <Select
-                title={<img src={avatar} alt="profile" />}
-                options={options}
-                containerClassName={s.select}
-                titleClassName={s.selectTitle}
-                optionClassName={'text-center'}
-              />
-              <Select title="" options={[]} containerClassName={s.selectDisabled} />
-            </div>
-          ) : (
-            <Button onClick={() => navigate(routes.auth)} text={'Войти'} className="links" />
-          )}
+          <NavBar />
         </div>
       </div>
       <div className={s.line}></div>
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 import { FC, useMemo } from 'react';
 
 import { motion, useReducedMotion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { useTypedSelector } from '../../hooks/redux';
 import { useAuth } from '../../hooks/useAuth';
@@ -12,21 +12,48 @@ import { routes } from '../../data';
 import s from './NavBar.module.scss';
 import Button from '../Button';
 import Logo from '../Logo';
+import { images } from '../../assets';
 
 interface ILink {
   to: routes;
   name: string;
+  isActive: boolean;
+  icon: typeof images.profile | null;
 }
 
-interface IProps {
-  links: ILink[];
-}
-
-const NavBar: FC<IProps> = ({ links }) => {
+const NavBar: FC = () => {
   const { isShow, setIsShow, ref } = useOutsideMenu();
-  const { isAuth } = useTypedSelector(state => state.auth);
+  const { pathname } = useLocation();
+  const { isAuth, currentUser } = useTypedSelector(state => state.auth);
   const { onLogOutBtnClick } = useAuth();
   const navigate = useNavigate();
+
+  const checkIsActive = (link: routes) => pathname === link;
+
+  const avatar = useMemo(
+    () => (!!currentUser?.photoURL ? currentUser.photoURL : images.profile),
+    [currentUser?.photoURL],
+  );
+
+  const links: ILink[] = useMemo(
+    () => {
+      const allRoutes: ILink[] = [
+        { to: routes.about, name: 'О NumBer', isActive: checkIsActive(routes.about), icon: null },
+        { to: routes.contacts, name: 'Наши контакты', isActive: checkIsActive(routes.contacts), icon: null },
+        { to: routes.catalog, name: 'Каталог', isActive: checkIsActive(routes.catalog), icon: null },
+      ];
+
+      const privateRoutes: ILink[] = [
+        { to: routes.profile, name: 'Аккаунт', isActive: checkIsActive(routes.profile), icon: null },
+        { to: routes.projects, name: 'Мои проекты', isActive: checkIsActive(routes.projects), icon: null },
+        { to: routes.exchangeRate, name: 'Курсы валют', isActive: checkIsActive(routes.exchangeRate), icon: null },
+      ];
+
+      return isAuth ? allRoutes.concat(privateRoutes) : allRoutes;
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [pathname],
+  );
 
   const handleClickNavbarLink = async (link: routes) => {
     setIsShow(false);
