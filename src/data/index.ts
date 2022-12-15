31b5c2c96486385212afd5be4d6536c7fd129aec
Maksim Attsetski <maks2001maks2001@gmail.com>
2022-12-15 23:29:28 +0300

    feature: add animations

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,6 +2,7 @@ import { quiz, systemsIds } from './quiz';
 import { routes } from './routes';
 import { materials } from './material';
 import { works } from './works';
+import { catalog } from './catalogItem';
 
-export { materials, works, routes, quiz };
+export { materials, works, routes, quiz, catalog };
 export type { systemsIds };
