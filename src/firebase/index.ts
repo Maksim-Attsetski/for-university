1d04a40195bed8f4bcda2f83cf05f21ed9b718e0
Maksim <98591524+Maksim-Attsetski@users.noreply.github.com>
2023-01-06 16:50:52 +0300

    Delete secret environment

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,14 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCI9g6baPTh3-WbSLhg3zPvkEC1s7F5Uno',
-  authDomain: 'admin-dashboard-74346.firebaseapp.com',
-  databaseURL: 'https://admin-dashboard-74346-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'admin-dashboard-74346',
-  storageBucket: 'admin-dashboard-74346.appspot.com',
-  messagingSenderId: '140561293917',
-  appId: '1:140561293917:web:6e19198ad14271f656a3b4',
-  measurementId: 'G-RR2K2QLJWZ',
+  apiKey: 'apiKey',
+  authDomain: 'authDomain',
+  databaseURL: 'databaseURL',
+  projectId: 'projectId',
+  storageBucket: 'storageBucket',
+  messagingSenderId: 'messagingSenderId',
+  appId: 'appId',
+  measurementId: 'measurementId',
 };
 
 // Initialize Firebase
